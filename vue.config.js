69d94050ead430289c6c9cb82e71e0246d6f58bd
DK <2632454453@qq.com>
2023-12-10 15:25:51 +0800

    更新配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
+//当 vue.config.js配置文件 配置的代理proxy是这样
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  publicPath: './',
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    host: 'localhost',
+    open: true,
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_REQUEST_URL,//'http://192.168.124.15:8000'
+        ws: true,
+        changOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+          //需要rewrite的,process.env.VUE_APP_BASE_API:'/dev-api'
+        }
+      }
+    }
+  },
 })
